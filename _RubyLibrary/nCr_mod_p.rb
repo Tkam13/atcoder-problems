ca6db15ac4aa4df7494235f72011e3933d07c06d
tomohiro <h1ro.kmd13@gmail.com>
2019-11-18 00:20:10 +0900

    fix code of abc145_d and nCr_mod_p Library

--- _RubyLibrary/nCr_mod_p.rb
+++ _RubyLibrary/nCr_mod_p.rb
@@ -1,25 +1,25 @@
-@fac = Array.new(MAX,0)
-@finv = Array.new(MAX,0)
-@inv = Array.new(MAX,0)
-MAX = 1000010
+class Combination_mod_p
+  def initialize(mod,max_num)
+    @max_num = max_num
+    @mod = mod
 
-def com_init(mod)
-  @fac[0] = @fac[1] = 1
-  @finv[0] = @finv[1] = 1
-  @inv[1] = 1
-  (2..MAX-1).each do |i|
-    @fac[i] = @fac[i-1]*i % mod
-    @inv[i] = mod - @inv[mod % i] * (mod / i) % mod
-    @finv[i] = @finv[i - 1] * @inv[i] % mod 
-  end
-end
+    @fac = Array.new(@max_num,0)
+    @finv = Array.new(@max_num,0)
+    @inv = Array.new(@max_num,0)
 
-def com(n,k,mod)
-  if n < k
-    return 0
+    @fac[0] = @fac[1] = 1
+    @finv[0] = @finv[1] = 1
+    @inv[1] = 1
+    (2..@max_num-1).each do |i|
+      @fac[i] = @fac[i-1]*i % @mod
+      @inv[i] = @mod - @inv[@mod % i] * (@mod / i) % @mod
+      @finv[i] = @finv[i - 1] * @inv[i] % @mod 
+    end
   end
-  if n < 0 || k < 0
-    return 0
+
+  def com(n,k)
+    return 0 if n < k || (n < 0 || k < 0)
+
+    return @fac[n] * (@finv[k] * @finv[n-k] % @mod) % @mod
   end
-  return @fac[n] * (@finv[k] * @finv[n-k] % mod) % mod
 end
--- abc145/d.rb
+++ abc145/d.rb
@@ -1,42 +1,43 @@
 x,y = gets.chomp.split.map(&:to_i)
 mod = 1000000007
 ans = 0
-MAX = 1000010
-@fac = Array.new(MAX,0)
-@finv = Array.new(MAX,0)
-@inv = Array.new(MAX,0)
+max_num = 1000010
 
-def com_init(mod)
-  @fac[0] = @fac[1] = 1
-  @finv[0] = @finv[1] = 1
-  @inv[1] = 1
-  (2..MAX-1).each do |i|
-    @fac[i] = @fac[i-1]*i % mod
-    @inv[i] = mod - @inv[mod % i] * (mod / i) % mod
-    @finv[i] = @finv[i - 1] * @inv[i] % mod 
+class Combination_mod_p
+  def initialize(mod,max_num)
+    @max_num = max_num
+    @mod = mod
+
+    @fac = Array.new(@max_num,0)
+    @finv = Array.new(@max_num,0)
+    @inv = Array.new(@max_num,0)
+
+    @fac[0] = @fac[1] = 1
+    @finv[0] = @finv[1] = 1
+    @inv[1] = 1
+    (2..@max_num-1).each do |i|
+      @fac[i] = @fac[i-1]*i % @mod
+      @inv[i] = @mod - @inv[@mod % i] * (@mod / i) % @mod
+      @finv[i] = @finv[i - 1] * @inv[i] % @mod 
+    end
+  end
+
+  def com(n,k)
+    return 0 if n < k || (n < 0 || k < 0)
+
+    return @fac[n] * (@finv[k] * @finv[n-k] % @mod) % @mod
   end
 end
-
-def com(n,k,mod)
-  if n < k
-    return 0
-  end
-  if n < 0 || k < 0
-    return 0
-  end
-  return @fac[n] * (@finv[k] * @finv[n-k] % mod) % mod
-end
-
-com_init(mod)
 
 if (-x + 2*y) % 3 == 0 && (2*x - y) % 3 == 0
+  combination = Combination_mod_p.new(mod,max_num)
   r_n = (-x + 2*y)/3
   u_n = (2*x - y)/3
   sum = r_n + u_n
   if r_n > u_n
-    ans = com(sum,u_n,mod)
+    ans = combination.com(sum,u_n)
   else
-    ans = com(sum,r_n,mod)
+    ans = combination.com(sum,r_n)
   end
 end
 
