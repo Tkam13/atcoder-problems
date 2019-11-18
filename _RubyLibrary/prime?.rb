3c8f1615ae073b618b78966eb1d79449d0d6326b
tomohiro <h1ro.kmd13@gmail.com>
2019-11-18 12:47:50 +0900

    add adc142_d and prime? Library

---
+++ _RubyLibrary/prime?.rb
@@ -0,0 +1,13 @@
+def prime?(n)
+  return true if n == 2 || n == 3
+  return false if n < 2 || n % 2 == 0 || n % 3 == 0
+  step_flag = true
+  current = 5
+  while current <= Math.sqrt(n).to_i
+    return false if n % current == 0
+    current += step_flag ? 2 : 4
+    step_flag = !step_flag
+  end
+
+  return true
+end
